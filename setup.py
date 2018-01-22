e82f6bf77c6757cb0992bc698aa9c8a03e27679b
Milinda Pathirage <milinda.pathirage@gmail.com>
2018-01-22 18:56:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
     name="pybuilder-external-plugin-demo",
-    version="0.2",
-    packages=find_packages(),
+    version="0.3",
     package_dir = {'': 'src/main/python'}
 )
